6d705f187688235e9a49f04eb31514fac860b291
chandanbandi <bchandu2988@gmail.com>
2020-01-11 14:25:01 +0530

    Updates

---
+++ precache-manifest.77a19545f6dec6ba91bbfd69404629e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b815426646f3c7352bc804d21955ff6b",
+    "url": "/pokemon-app/index.html"
+  },
+  {
+    "revision": "c4ae3fd01ee3b5f303a8",
+    "url": "/pokemon-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6753478b6803184e66d9",
+    "url": "/pokemon-app/static/css/main.5fb7a6d0.chunk.css"
+  },
+  {
+    "revision": "c4ae3fd01ee3b5f303a8",
+    "url": "/pokemon-app/static/js/2.1e111ea9.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/pokemon-app/static/js/2.1e111ea9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6753478b6803184e66d9",
+    "url": "/pokemon-app/static/js/main.1011788d.chunk.js"
+  },
+  {
+    "revision": "9013913bce122dc3981b",
+    "url": "/pokemon-app/static/js/runtime-main.907950ba.js"
+  }
+]);
